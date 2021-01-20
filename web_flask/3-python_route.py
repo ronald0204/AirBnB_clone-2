473169b68b5beaf662245026d270e66e8465260e
ronald0204 <ronaldaguirre0204@gmail.com>
2021-01-20 15:55:28 +0000

    Task #3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""starts a Flask web application
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """starts a Flask web application
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """starts a Flask web application
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """followed by the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>', strict_slashes=False)
+def python_is_fun(text='is cool'):
+    """ followed by the value of the text python"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
