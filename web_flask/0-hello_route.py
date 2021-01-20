f1f346a7af90e8946a8a825d158a508c79f6986a
ronald0204 <ronaldaguirre0204@gmail.com>
2021-01-20 09:51:13 -0500

    Task #0.1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,14 +4,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
-    """ 
+    """
         Keyword arguments:
         argument -- route the flask
         Return: name (html)
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
